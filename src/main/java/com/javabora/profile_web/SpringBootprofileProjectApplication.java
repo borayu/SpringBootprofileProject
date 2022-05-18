9a7ea147e3051f24b64b9618adb99a6b58f0fc4d
ici906 <ici906@DESKTOP-GMISII5>
2022-05-18 15:18:19 +0900

    SpringBootprofileProject

---
+++ src/main/java/com/javabora/profile_web/SpringBootprofileProjectApplication.java
@@ -0,0 +1,13 @@
+package com.javabora.profile_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootprofileProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootprofileProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javabora/profile_web/SpringBootprofileProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabora.profile_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootprofileProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
